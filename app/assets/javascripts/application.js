52654d87a4cdf7df81ab06d63f2abca17cb0fee1
tony <tonyapp5013@gmail.com>
2013-11-01 01:56:05 -0700

    Added realtime rendering of user supplied markdown content

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,31 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require marked
 //= require_tree .
+
+// Set default options except highlight which has no default
+marked.setOptions({
+  gfm: true,
+  highlight: function (code, lang, callback) {
+    pygmentize({ lang: lang, format: 'html' }, code, function (err, result) {
+      if (err) return callback(err);
+      callback(null, result.toString());
+    });
+  },
+  tables: true,
+  breaks: true,
+  pedantic: false,
+  sanitize: true,
+  smartLists: true,
+  smartypants: false,
+  langPrefix: 'lang-'
+});
+
+$(document).ready(function() {
+  $( "textarea" ).keyup(function() {
+    var value = $( this ).val();
+    $( ".marked-text" ).html( marked(value) );
+  })
+  .keyup();
+});
