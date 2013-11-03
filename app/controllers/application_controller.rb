328e060a89416dadc0b128b975fd59556250b098
Tony Correia <TonyAPP5013@gmail.com>
2013-11-03 09:38:10 +0000

    Added mailer settings to production.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
 
 
 def after_sign_in_path_for(resource)
-  new_page_path
+  '/pages/index'
 end
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,9 @@ Docshare::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+
+config.action_mailer.default_url_options = { :host => 'docshare.herokuapp.com/.com' }
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
